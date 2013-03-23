3ffe428eae4b8db4a22c0197fbc25396c7a7cae7
Dennis Riis <dr@driis.dk>
2013-03-23 19:53:00 +0100

    GraphViz: Don't repeat nodes, use a better label.

--- GraphvizWriter.cs
+++ GraphvizWriter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace NuGetPackageVisualizer
@@ -17,19 +18,28 @@ namespace NuGetPackageVisualizer
             var sb = new StringBuilder();
             sb.AppendLine("digraph packages {");
             sb.AppendLine(" node [shape=box, style=\"rounded,filled\"]");
+
+            var packageLookup = packages.ToDictionary(x => x.GraphId());
+
             foreach (var package in packages)
             {                                
-                sb.Append(" ").AppendFormat("\"{0}\"[fillcolor=\"{1}\"]", 
-                    package.NugetId,
-                    GraphHelper.GenerateBackgroundColor(packages, package, colors)).AppendLine();
+                sb.AppendFormat(" \"{0}\"[fillcolor=\"{1}\",label=\"{2}\"]", 
+                    package.GraphId(),
+                    GraphHelper.GenerateBackgroundColor(packages, package, colors),
+                    package.DisplayVersion()).AppendLine();
+
+                foreach (var dep in package.Dependencies.Where(d => !packageLookup.ContainsKey(d.GraphId())))
+                {
+                    sb.AppendFormat("\"{0}\"[label=\"{1}\"]", dep.GraphId(), dep.DisplayVersion());                    
+                }
 
                 foreach (var dep in package.Dependencies)
                 {
                     sb.Append(" ")
                       .Append("\"")
-                      .Append(package.NugetId)
+                      .Append(package.GraphId())
                       .Append("\" -> \"")
-                      .Append(dep.NugetId)
+                      .Append(dep.GraphId())
                       .AppendLine("\"");
                 }
             }
